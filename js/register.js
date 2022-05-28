2d0f807dc79682e3d940f66a59656aa7a150abc4
Elisa <aerodriguez4@gmail.com>
2022-05-28 06:36:32 +0200

    Puerta Mágica

--- js/register.js
+++ js/register.js
@@ -2,26 +2,33 @@ $(document).ready(function(){
     $("#btn-login").click(function(){
         let username = $("#email").val().trim();
         let password = $("#pwd").val().trim();
+        let confpassword = $("#confpwd").val().trim();
 
         console.log(username)
 
-        if( username != "" && password != "" ){
-            $.ajax({
-                url:'http://localhost:5002/Alumno/Login',
+        if( username != "" && password != "" && confpassword != "" ){
+            if (password == confpassword) {
+                $.ajax({
+                url:'http://localhost:5002/User/Reg',
                 type:'post',
                 data:JSON.stringify({email:username,password:password}),
                 contentType:"application/json; charset=utf-8",
                 success:function(response){
                     
-                    window.location = "welcome.html";
+                    window.location = "index.html";
                 },
                 error:function(){
                     $("#error").fadeIn();
                 }
             });
+        } else {
+            alert("Complete todos los campos.");
         }
+            }
+
     });
-    $("#btn-register").click(function(){
-        window.location = "register.html";
+
+    $("#btn-volver").click(function(){
+        window.location = "index.html";
     });
 });
--- js/welcome.js
+++ js/welcome.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
         
     $.ajax({
-        url:'https://localhost:5001/Alumno/All',
+        url:'http://localhost:5002/User/All',
         type:'get',
         contentType:"application/json; charset=utf-8",
         success:function(response){
-            let alumnos = response
-            alumnos.forEach(alumno => {
-                console.log(alumno)
-                $("#alumnos").append('<li>'+alumno.dni+'</li>')
+            let users = response
+            users.forEach(user => {
+                console.log(user)
+                $("#users").append('<li>'+user.Username+'</li>')
             });
             
         },
